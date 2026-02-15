6ffd673d997eef42876a0fb297ee24d83ba81a7d
Pawanks7861 <happyk682@gmail.com>
2026-02-15 17:50:24 +0530

    first commit

---
+++ application/logs/log-2026-02-15.php
@@ -0,0 +1,22 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2026-02-15 11:18:05 --> Severity: Warning --> include_once(C:\wamp64\www\phoenixerp\application\hooks./../libraries/App_modules.php): Failed to open stream: Permission denied C:\wamp64\www\phoenixerp\application\hooks\InitModules.php 12
+ERROR - 2026-02-15 11:18:05 --> Severity: Warning --> include_once(): Failed opening 'C:\wamp64\www\phoenixerp\application\hooks./../libraries/App_modules.php' for inclusion (include_path='.;C:\php\pear') C:\wamp64\www\phoenixerp\application\hooks\InitModules.php 12
+ERROR - 2026-02-15 11:18:05 --> Severity: Warning --> include_once(C:\wamp64\www\phoenixerp\application\hooks./../../system/helpers/directory_helper.php): Failed to open stream: Permission denied C:\wamp64\www\phoenixerp\application\hooks\InitModules.php 14
+ERROR - 2026-02-15 11:18:05 --> Severity: Warning --> include_once(): Failed opening 'C:\wamp64\www\phoenixerp\application\hooks./../../system/helpers/directory_helper.php' for inclusion (include_path='.;C:\php\pear') C:\wamp64\www\phoenixerp\application\hooks\InitModules.php 14
+ERROR - 2026-02-15 11:18:05 --> Severity: error --> Exception: Class "App_modules" not found C:\wamp64\www\phoenixerp\application\hooks\InitModules.php 16
+ERROR - 2026-02-15 11:21:10 --> 404 Page Not Found: /index
+ERROR - 2026-02-15 11:21:13 --> 404 Page Not Found: /index
+ERROR - 2026-02-15 11:21:16 --> 404 Page Not Found: /index
+ERROR - 2026-02-15 11:21:29 --> 404 Page Not Found: /index
+ERROR - 2026-02-15 11:21:33 --> 404 Page Not Found: /index
+ERROR - 2026-02-15 16:51:36 --> Could not find the language line "RFI"
+ERROR - 2026-02-15 16:51:36 --> Could not find the language line "RFI"
+ERROR - 2026-02-15 16:51:38 --> Could not find the language line "appointment_new_appointment_submitted"
+ERROR - 2026-02-15 11:21:41 --> 404 Page Not Found: /index
+ERROR - 2026-02-15 16:51:42 --> Could not find the language line "RFI"
+ERROR - 2026-02-15 16:51:42 --> Could not find the language line "RFI"
+ERROR - 2026-02-15 16:51:42 --> Could not find the language line "RFI"
+ERROR - 2026-02-15 16:51:42 --> Could not find the language line "RFI"
+ERROR - 2026-02-15 16:51:42 --> Could not find the language line "RFI"
+ERROR - 2026-02-15 16:51:42 --> Could not find the language line "RFI"
